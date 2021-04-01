a0c4b5f3feb3bf907d4645936534d208dcaa9e4d
7fi <7fi@7fi.com>
2021-03-31 20:19:05 -0700

    dipperly

--- js/music.js
+++ js/music.js
@@ -4,7 +4,8 @@ const music = () => {
     musicPlaying = true;
     let backAudio = document.createElement('audio');
     backAudio.autoplay = true;
-    backAudio.setAttribute('src', 'https://krunker.io/sound/ambient_9.mp3');
+    // backAudio.setAttribute('src', 'https://krunker.io/sound/ambient_9.mp3');
+    backAudio.setAttribute('src','/sound/dipperly.mp3');
     backAudio.volume = 0.1;
     backAudio.play();
     backAudio.addEventListener('ended', function() {
